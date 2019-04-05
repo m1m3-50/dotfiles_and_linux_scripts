cfb6c0b9dc2cf2657fa5f8b2d67fc108f3d33e30
Aleksander Zień <Aleks@nderZien.com>
2019-04-05 18:24:30 +0200

    Add start_basics to launch layout + overall minor improvements

---
+++ .config/i3/start_basics.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+sleep 1
+
+# Workspace 1
+urxvt -e bash -c 'neofetch && bash' &
+urxvt -e bash -c 'newsboat' &
+
+# Workspace 5
+urxvt -T IRC -e bash -c irssi &
+
+# Workspace 9
+urxvt -T gotop -e bash -c gotop &
+
+# Workspace 10
+urxvt -T wifi -e bash -c wicd-curses &
+urxvt -T matrix -e bash -c cmatrix &
+pavucontrol &
+pulseeffects &
+i3-msg 'workspace 1;' &
